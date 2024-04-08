f9ffb0d5ccf360cc6ec3e702f53742567c70894c
Whalther <whaltherk@gmail.com>
2024-04-08 18:40:36 -0400

    Practica QGIS_1

---
+++ layers/layers.js
@@ -0,0 +1,17 @@
+var wms_layers = [];
+
+
+        var lyr_OSMStandard_0 = new ol.layer.Tile({
+            'title': 'OSM Standard',
+            //'type': 'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+    attributions: ' &middot; <a href="https://www.openstreetmap.org/copyright">© OpenStreetMap contributors, CC-BY-SA</a>',
+                url: 'http://tile.openstreetmap.org/{z}/{x}/{y}.png'
+            })
+        });
+
+lyr_OSMStandard_0.setVisible(true);
+var layersList = [lyr_OSMStandard_0];
